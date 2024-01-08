bf2e8b425713431f7804eb01a69ad11deeaaf9cd
Tariq Zaitoun <tariq.s.zaitoun@gmail.com>
2024-01-07 19:17:11 -0500

    Initial Commit

---
+++ src/main/java/com/tariq/rest/webservices/restfulweb0services/RestfulWeb0servicesApplication.java
@@ -0,0 +1,13 @@
+package com.tariq.rest.webservices.restfulweb0services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWeb0servicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWeb0servicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tariq/rest/webservices/restfulweb0services/RestfulWeb0servicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tariq.rest.webservices.restfulweb0services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWeb0servicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
